c956b398d5e63568215e63281636c06286a6b156
RuthCodina <ruth0066@hotmail.com>
2022-06-10 14:12:25 -0500

    fourth commit

--- jeddi2.go
+++ jeddi2.go
@@ -47,11 +47,46 @@ func main() {
 		Have it print out the years you have been alive.
 
 	*/
-	bd := 2000
+	/*bd := 2000
 	for {
 		if bd <= 2022 {
 			fmt.Println(bd)
 			bd++
 		}
+	}
+	*/
+	/*
+		Print out the remainder (modulus) which is found for each number between 10 and 100 when it is divided by 4
+	*/
+	/*
+		for i := 0; i <= 100; i++ {
+			fmt.Println(i % 4)
+		}
+	*/
+	/*
+		Create a program that shows the “if statement” in action
+	*/
+	/*
+		b := true
+		if reflect.TypeOf(b) == reflect.TypeOf("") {
+			fmt.Print("es un string")
+		} else if reflect.TypeOf(b) == reflect.TypeOf(0) {
+			fmt.Print("es un number")
+		} else {
+			fmt.Print("es un booleano")
+		}
+	*/
+	/*
+		Create a program that uses a switch statement with the switch expression specified as a
+		variable of TYPE string with the IDENTIFIER “favSport”.
+	*/
+	favsport := ""
+	switch favsport {
+	case "tennis":
+		fmt.Print("the tennis is fav")
+	case "soccer":
+		fmt.Print("the soccer is fav")
+	default:
+		fmt.Print("no hay fav")
 	}
 }
